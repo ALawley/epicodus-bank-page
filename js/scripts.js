f7f24e6dc2cbdadf7c4e63bb1ca680026a518c12
Anna Lawley and Jess Guilford <jess.guilford@gmail.com>
2016-01-26 13:26:04 -0800

    build barebones front end for single account

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,33 @@ BankAccount.prototype.withdraw = function(withdrawal) {
 BankAccount.prototype.deposit = function(deposited) {
   this.balance = this.balance + deposited;
 }
+
+$(document).ready(function() {
+  $("form#account-signup").submit(function(event) {
+    event.preventDefault();
+    var accountName = $("input#accountname").val();
+    var initialDeposit = parseInt($("input#initialdeposit").val());
+
+    var newAccount = new BankAccount(accountName, initialDeposit);
+
+    $(".account-name").text(newAccount.accountName);
+    $(".account-balance").text(newAccount.balance);
+
+    $("form#withdraw").submit(function(event) {
+      event.preventDefault();
+      var withdrawAmount = parseInt($("input#withdrawal").val());
+      newAccount.withdraw(withdrawAmount);
+      $(".account-balance").text(newAccount.balance);
+      $("input#withdrawal").val("");
+    });
+    $("form#deposit").submit(function(event) {
+      event.preventDefault();
+      var depositAmount = parseInt($("input#deposited").val());
+      newAccount.deposit(depositAmount);
+      $(".account-balance").text(newAccount.balance);
+      $("input#deposited").val("");
+    });
+  });
+
+
+});
